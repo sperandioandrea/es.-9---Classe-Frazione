3f0414697935cfd0d32ff7c095d58765ed5c0a86
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2023-10-31 13:01:59 +0100

    Creazione classe Frazione

---
+++ es. 9 -  Classe Frazione/Frazione.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._9____Classe_Frazione
+{
+    internal class Frazione
+    {
+    }
+}
