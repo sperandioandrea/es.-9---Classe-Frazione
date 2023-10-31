faa5f977dba819ce46c627edf30590b86e97191a
Sperandio.20200 <Sperandio.20200@E2-9-PC12>
2023-10-31 13:24:57 +0100

    Metodi Get e Set

--- es. 9 -  Classe Frazione/Frazione.cs
+++ es. 9 -  Classe Frazione/Frazione.cs
@@ -18,8 +18,18 @@ namespace es._9____Classe_Frazione
             numeratore = 0;
             denominatore = 0;
         }
-        
-       
+        //metodi Get e Set
+        public int Numeratore
+        {
+            get { return numeratore; }
+            set { numeratore = value; }
+        }
+        public int Denominatore
+        {
+            get { return denominatore; }
+            set { denominatore = value; }
+        }
+
 
 
     }
